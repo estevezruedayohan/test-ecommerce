ed33100f263e7b0e74e2affca2b4ac904556f038
Yohan Manuel Estevez Rueda <estevezruedayohan@gmail.com>
2022-09-26 23:03:45 -0500

    [add] file example, [create] file services index endpoints API

---
+++ src/services/api/index.js
@@ -0,0 +1,35 @@
+const API = process.env.NEXT_PUBLIC_API_URL;
+const VERSION = process.env.NEXT_PUBLIC_API_VERSION;
+
+
+const endPoints = {
+  auth: {
+    login: `${API}/api/${VERSION}/auth/login`,
+    profile: `${API}/api/${VERSION}/auth/profile`,
+  },
+  products:{
+    listProducts: (limit, offset) => `${API}/api/${VERSION}/products?limit=${limit}&offset=${offset}`,
+    getProduct: (id) => `${API}/api/${VERSION}/products/${id}`,
+    // addProduct: (product) => `${API}/api/${VERSION}/products`,
+    updateProducto: (id) => `${API}/api/${VERSION}/products/${id}`,
+    deleteProducto: (id) => `${API}/api/${VERSION}/products/${id}`,
+  },
+  users:{
+    getUsers: (limit) => `${API}/api/${VERSION}/users?limit=${limit}`,
+    // createUser: (user) => `${API}/api/${VERSION}/users`,
+    // doExist: (email) => `${API}/api/${VERSION}/users/is-available`,
+  },
+  categories:{
+    listCategories: (limit) => `${API}/api/${VERSION}/categories?limit=${limit}`,
+    // addCategorie: (categorie) => `${API}/api/${VERSION}/categories`,
+    getCategorie: (id) => `${API}/api/${VERSION}/categories/${id}`,
+    updateCategorie: (id) => `${API}/api/${VERSION}/categories/${id}`,
+    // filterProductByCategorie: (idCat,limit,offset) => `${API}/api/${VERSION}/categories/${idCat}/products?limit=${limit}?offset=${offset}`,
+  },
+  files:{
+    getFile: (fileName) => `${API}/api/${VERSION}/files/${fileName}`,
+
+  },
+};
+
+export default endPoints;
