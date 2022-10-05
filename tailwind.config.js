425cff3754a9d13a0340cce505935e2e5235b937
Yohan Manuel Estevez Rueda <estevezruedayohan@gmail.com>
2022-10-05 11:02:38 -0500

    [create] alert file use alert

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require('tailwindcss/colors');
 
+delete colors['lightBlue'];
+delete colors['coolGray'];
+delete colors['blueGray'];
+delete colors['warmGray'];
+delete colors['trueGray'];
+
 module.exports = {
   content: [
     './src/**/*{html,js,jsx}'
